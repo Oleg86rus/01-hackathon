811761b4d40b932df4d0f031dec7daf48d0ab9ab
Sergey Fomin <gray7410@yandex.ru>
2022-03-13 22:02:09 +0500

    to bottom left

--- src/context_menu/modules/custom_text.module.js
+++ src/context_menu/modules/custom_text.module.js
@@ -11,12 +11,16 @@ export class CustomText extends Module {
     trigger() {
         return new Promise(() => {
             const menuItem = document.querySelector("[data-type = 'customText']");
+            const messageBox = document.createElement('ul')
+            messageBox.classList.add('message-box')
+            document.body.append(messageBox)
             const POSTS_URL = 'https://my-json-server.typicode.com/Oleg86rus/01-hackathon/custom-message'
             menuItem.addEventListener('click', () => {
                 const createPostItem = text => {
-                    const blockItem = document.createElement('div');
+                    const blockItem = document.createElement('li');
                     blockItem.classList.add('blockItem')
-                    document.body.append(blockItem)
+                    console.log('messageBox', messageBox)
+                    messageBox.append(blockItem)
                     blockItem.innerText = text
                     setTimeout(function(){
                         blockItem.remove()
